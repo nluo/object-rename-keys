7fc752ca61c915df086cbc7fcbeb1fd484da5696
Nick Luo <nick.luo@me.com>
2015-06-28 22:32:03 +1000

    Updated usage.js

--- usage.js
+++ usage.js
@@ -1,45 +1,19 @@
 var objectRenameKeys = require('./');
 
-var address = 
-{ 	
-	display: 'yes',
-	streetNumber: '39',
-	street: 'Hollywood Street',
-	suburb: 'Calamvale',
-	state: 'QLD',
-	postcode: '4109',
-	country: 'AUS' 
+var majigger = {
+    address:
+	{
+        	street: 'Hollywood Street',
+		suburb: 'Calamvale',
+		country: 'AUS'
+	}
 };
 
 var changes = {
-	streetNumber: 'addressStreetNumber',
-	street: 'addressStreet',
-	suburb: 'Suburb',
-	state: 'State',
-	country: 'Country'
-}
-
-console.log(objectRenameKeys(address, changes));
-
-
-// reset the address
-address = null;
-
-address = 
-{ 	
-	display: 'yes',
-	streetNumber: '39',
-	street: 'Hollywood Street',
-	suburb: 'Calamvale',
-	state: 'QLD',
-	postcode: '4109',
-	country: 'AUS' 
+  address: {
+  	street: 'myStreet'
+  },
+  suburb: 'Suburb'
 };
 
-// It supports nested object too
-
-var property = {};
-
-property.address = address;
-
-console.log(objectRenameKeys(property, changes));
+console.log(objectRenameKeys(majigger, changes));
