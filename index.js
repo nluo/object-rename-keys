c58380c33e59a59041a6f5b4397753b3f95479be
Jelle Breuer <jelle@breuclan.nl>
2018-02-25 15:55:05 +0100

    Added option to pass an array of objects

--- index.js
+++ index.js
@@ -1,33 +1,39 @@
-var clone = require('clone');
+var clone = require('clone')
 
-function renameKeys(object, changes){
-	if (!changes || typeof changes !== 'object') {
-		return object;
-	}
+function renameKeys (object, changes) {
+  if (!changes || typeof changes !== 'object') {
+    return object
+  }
 
-	if (Array.isArray(object)){
-		return object;
-	}
+  if (Array.isArray(object)) {
+    const newArray = []
+    for (const obj of object) {
+      newArray.push(renameKeys(obj, changes))
+    }
+    return newArray
+  } else {
+    if (typeof object !== 'object') {
+      return object
+    }
+    var copy = clone(object)
 
-	var copy = clone(object);
+    for (var key in changes) {
+      if (typeof changes[key] === 'object') {
+        if (copy.hasOwnProperty(key)) {
+          copy[key] = renameKeys(copy[key], changes[key])
+          continue
+        }
+      }
 
-	for (var key in changes) {
-		if (typeof changes[key] === 'object') {
-			if (copy.hasOwnProperty(key)) {
-				// traverse again
-				copy[key] = renameKeys(copy[key], changes[key]);
-				continue;
-			}
-		}
+      if (copy.hasOwnProperty(key)) {
+        var temp = copy[key]
 
-		if (copy.hasOwnProperty(key)) {
-			var temp = copy[key];
-
-			copy[changes[key]] = temp;
-			delete copy[key];
-		}
-	}
-	return copy;
+        copy[changes[key]] = temp
+        delete copy[key]
+      }
+    }
+    return copy
+  }
 }
 
-module.exports = renameKeys;
+module.exports = renameKeys
