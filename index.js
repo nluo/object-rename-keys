763fb7658a431392b0eca4839cc742df9e3c6089
Nick Luo <nick.luo@me.com>
2018-03-22 14:25:44 +1000

    WIP

--- index.js
+++ index.js
@@ -1,39 +1,43 @@
-var clone = require('clone')
+'use strict';
+
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
+var clone = require('clone');
 
 function renameKeys(object, changes) {
-  if (!changes || typeof changes !== 'object') {
-    return object
+  if (!changes || (typeof changes === 'undefined' ? 'undefined' : _typeof(changes)) !== 'object') {
+    return object;
   }
 
   if (Array.isArray(object)) {
-    const newArray = []
-    for (var i = 0; i< object.length; i++) {
-      newArray.push(renameKeys(object[i], changes))
+    var newArray = [];
+    for (var i = 0; i < object.length; i++) {
+      newArray.push(renameKeys(object[i], changes));
     }
-    return newArray
+    return newArray;
   } else {
-    if (typeof object !== 'object') {
-      return object
+    if ((typeof object === 'undefined' ? 'undefined' : _typeof(object)) !== 'object') {
+      return object;
     }
-    var copy = clone(object)
+    var copy = clone(object);
 
     for (var key in changes) {
-      if (typeof changes[key] === 'object') {
+      if (_typeof(changes[key]) === 'object') {
         if (copy.hasOwnProperty(key)) {
-          copy[key] = renameKeys(copy[key], changes[key])
-          continue
+          copy[key] = renameKeys(copy[key], changes[key]);
+          continue;
         }
       }
 
       if (copy.hasOwnProperty(key)) {
-        var temp = copy[key]
+        var temp = copy[key];
 
-        copy[changes[key]] = temp
-        delete copy[key]
+        copy[changes[key]] = temp;
+        delete copy[key];
       }
     }
-    return copy
+    return copy;
   }
 }
 
-module.exports = renameKeys
+module.exports = renameKeys;
---
+++ src/index.js
@@ -0,0 +1,31 @@
+var clone = require('clone')
+
+function renameKeys(object, changes) {
+  if (!changes || typeof changes !== 'object') {
+    return object
+  }
+
+  if (typeof object !== 'object') {
+    return object
+  }
+  var copy = clone(object)
+
+  for (var key in changes) {
+    if (typeof changes[key] === 'object') {
+      if (copy.hasOwnProperty(key)) {
+        copy[key] = renameKeys(copy[key], changes[key])
+        continue
+      }
+    }
+
+    if (copy.hasOwnProperty(key)) {
+      var temp = copy[key]
+
+      copy[changes[key]] = temp
+      delete copy[key]
+    }
+  }
+  return copy
+}
+
+module.exports = renameKeys
